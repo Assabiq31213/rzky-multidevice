e199f294b41b53a15a72fb55b365a537cd3f1735
assabiq <82302556+Assabiq31213@users.noreply.github.com>
2022-08-05 10:03:48 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ const limitData = cron.schedule(
 let data = fs.readFileSync(path.join(__dirname, "doom.flf"), "utf8");
 require("figlet").parseFont("doom", data);
 require("figlet").text(
-	"RZKY MD",
+	"SAKURA MD",
 	{
 		font: "doom",
 		horizontalLayout: "default",
