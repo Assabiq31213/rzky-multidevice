cfbf533c1af959655c774b3887c8accc5ab28d15
assabiq <82302556+Assabiq31213@users.noreply.github.com>
2022-08-07 22:06:37 +0700

    Add files via upload

--- command/asupan/asupansantuy.js
+++ command/asupan/asupansantuy.js
@@ -7,10 +7,11 @@ module.exports = {
 	isSpam: true,
 	async run({ msg, conn }) {
 	    var ini = await fetchJson(`https://ziy.herokuapp.com/api/asupan/santuy?apikey=xZiyy`)
+	    buff = ini.result.url
 		await msg.reply(response.wait);
 		const buttons = [{ buttonId: "#asupansantuy", buttonText: { displayText: "Get Again" }, type: 1 }];
 		const buttonMessage = {
-			video: { url: ini.result.url },
+			video: { url: buff },
 			caption: "Nih kak",
 			footer: "Sakura-Botz - By BiqqTzy",
 			buttons: buttons,
